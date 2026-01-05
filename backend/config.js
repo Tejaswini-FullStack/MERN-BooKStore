5e65f8e61081d959d88843a18fef74d37de43962
Tejaswini-FullStack <tejaspathireddy@gmail.com>
2026-01-05 11:16:38 +0530

    Fix backend config using dotenv and env variables

--- backend/config.js
+++ backend/config.js
@@ -4,8 +4,9 @@
 // console.log("ENV CHECK:", process.env.MONGODB_URL);
 
 // export const PORT = process.env.PORT;
-export const PORT=5555;
+import dotenv from "dotenv";
 
+dotenv.config();
 
-export const mongoDBURL =
-'mongodb+srv://root:<password>@books-store-mern.pznccyn.mongodb.net/books-collection?appName=Books-Store-MERN'
+export const PORT = process.env.PORT || 5555;
+export const mongoDBURL = process.env.MONGO_URI;
